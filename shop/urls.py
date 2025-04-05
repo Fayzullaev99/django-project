53326e1169a61580581bd88ee0ac6bd84a164531
Sanjar <sanjarfayzullaev99@gmail.com>
2025-04-05 21:05:18 +0500

    auth finished

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from shop.views import index
+from shop.views import *
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("signin/", signin, name="signin"),
+    path("signup/", signup, name="signup"),
+    path("signout/", signout, name="signout"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView
+from django.contrib.auth import logout, authenticate, login
+from shop.forms import SignInForm, SignUpForm
 
 # Create your views here.
 
@@ -8,3 +10,43 @@ from django.views.generic import ListView
 
 def index(request):
     return render(request, "shop/index.html")
+
+def signup(request):
+    form = SignUpForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(request, username=email, password=password)
+            if user:
+                login(request,user)
+                return redirect("index")
+    context = {
+        "title":"Sign Up",
+        "form":form
+    }
+    return render(request, "shop/signup.html", context)
+
+def signin(request):
+    form = SignInForm(data=request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user:
+                login(request,user)
+                return redirect("index")
+    context = {
+        "title":"Sign In",
+        "form":form
+    }
+    return render(request, "shop/signin.html", context)
+
+def signout(request):
+    logout(request)
+    return redirect("signin")
+
+
+
